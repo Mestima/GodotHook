564ddd4aaae05da892a11118d406dc9fc44f2728
Mestima <mestima@nilored.ru>
2022-05-17 23:46:38 +0300

    Module path renamed: hook -> GodotHook

--- register_types.cpp
+++ register_types.cpp
@@ -4,13 +4,13 @@
 #include "core/object/class_db.h"
 #include "hook.h"
 
-void initialize_hook_module(ModuleInitializationLevel p_level) {
+void initialize_GodotHook_module(ModuleInitializationLevel p_level) {
 	if (p_level == MODULE_INITIALIZATION_LEVEL_SCENE) {
 		ClassDB::register_class<Hook>();
 	}
 }
 
-void uninitialize_hook_module(ModuleInitializationLevel p_level) {
+void uninitialize_GodotHook_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
--- register_types.h
+++ register_types.h
@@ -2,6 +2,6 @@
 
 #include "modules/register_module_types.h"
 
-void initialize_hook_module(ModuleInitializationLevel p_level);
-void uninitialize_hook_module(ModuleInitializationLevel p_level);
+void initialize_GodotHook_module(ModuleInitializationLevel p_level);
+void uninitialize_GodotHook_module(ModuleInitializationLevel p_level);
 /* yes, the word in the middle must be the same as the module folder name */
